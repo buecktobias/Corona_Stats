00d2fd7b89d4b4547995521f02996bfbe0e628b9
tobias <buecktobias@gmail.com>
2020-03-31 15:15:29 +0200

    created colors class which saves the colors

---
+++ m_statistics/colors.py
@@ -0,0 +1,9 @@
+from Corona_Stats import settings as settings
+import os
+
+class Color:
+    def __init__(self):
+        self.brand_color = "#47121a"
+        self.cases_color = "#e6c346"
+        self.active_cases_color = "#fff300"
+        self.recovered_color = "#3bff00"
